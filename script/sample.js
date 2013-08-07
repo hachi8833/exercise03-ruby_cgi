ad7f4d32450bc8c7abec7c01b2e8ee7c589ec980
shozo hatta <hachi8833@gmail.com>
2013-08-07 20:41:23 +0900

    Add a basic presence check with javascript

--- script/sample.js
+++ script/sample.js
@@ -1,8 +1,18 @@
-$(function(){
-  $("#file_01").change(function(){
-    $("#mask_file_01").val($("#file_01").val());
-  });
-  $("#mask_file_01").click(function(){
-    $("#file_01").click();
-  });
-});
+function check() {
+  var message = [];
+
+  if(document.form1.email.value == "") {
+    message.push("・メールアドレスが入力されていません。\n");
+  }
+  if(document.form1.reason.value == ""){
+    message.push("・志望動機が入力されていません。\n");
+  }
+    message =  message.join("");
+  if(message != "") {
+    alert("以下の項目を修正してください。\n\n" + message);
+    return false;
+  }
+  else {
+    return true;
+  }
+}
